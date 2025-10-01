614dd214509b77ded580cf72de9b461a4133b5a7
Pristine duru <Favourduru24@gmail.com>
2025-09-30 20:29:03 -0700

    finish implementing the sign-up page uiux

--- src/app/(root)/sign-in/page.jsx
+++ src/app/(root)/sign-in/page.jsx
@@ -1,7 +1,43 @@
+'use client'
+import Image from "next/image"
+import Link from "next/link"
+
 const SignIn = () => {
+
    return (
-     <div>
-        SignIn
+     <div className="auth">
+        <section className="size-full glassmorphism flex-center px-6">
+           <div className="sign-in-card">
+               <header className="header">
+                 <Link href="/">
+                     <Image 
+                       src="/assets/icons/logo.svg"
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className="size-[30px]"
+/>
+                 </Link>
+                 <h1 className="p-28-bold text-dark-100">Tourvisto</h1>
+               </header>
+
+               <article>
+                 <h2 className="p-28-semibold text-dark-100 text-center">Start Your Travel journey</h2>
+                 <p className="p-18-regular text-center text-gray-100 !leading-7">Sign in with Google to manage destinations, itineraties, and user activity with ease</p>
+               </article>
+
+               <button className="button-class !h-11 !w-full" onClick={() => {}}>
+                     <Image 
+                       src="/assets/icons/google.svg"
+                        alt="google"
+                        width={24}
+                        height={24}
+                        className="size-5"
+                         />
+                   <span className="p-18-semibold text-white">Sign in with Google</span>
+               </button>
+           </div>
+        </section>
      </div>
    )
 }
